7d36f60f142535d9a5a81a245ec75e89253860e3
xant <xant@xant.net>
2014-04-01 08:35:24 +0200

    minor-fix : ensure null-terminating the buffer after a copy
    (not really necessary but expected)

--- src/fbuf.c
+++ src/fbuf.c
@@ -360,6 +360,7 @@ fbuf_copy(fbuf_t *fbufsrc, fbuf_t *fbufdst)
     fbuf_clear(fbufdst);
     memcpy(fbufdst->data + fbufdst->skip, fbufsrc->data + fbufsrc->skip, fbufsrc->used);
     fbufdst->used = fbufsrc->used;
+    fbufdst->data[fbufdst->skip+fbufdst->used] = 0;
 
     return fbufdst->used;
 }
