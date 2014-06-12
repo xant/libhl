40ec5aa8f5b2f904b872a0aff3a9304315b614f5
Andrea Guzzo <xant@xant.net>
2014-06-12 13:18:25 +0200

    removed an useless conditional + fetch the previous value before incrementing/decrementing

--- src/atomic_defs.h
+++ src/atomic_defs.h
@@ -2,20 +2,18 @@
 #define __ATOMIC_DEFS_H__
 
 #define ATOMIC_READ(__v) __sync_add_and_fetch(&(__v), 0)
-#define ATOMIC_INCREMENT(__v) (void) __sync_add_and_fetch(&(__v), 1)
-#define ATOMIC_DECREMENT(__v) (void) __sync_sub_and_fetch(&(__v), 1)
+#define ATOMIC_INCREMENT(__v) (void) __sync_fetch_and_add(&(__v), 1)
+#define ATOMIC_DECREMENT(__v) (void) __sync_fetch_and_sub(&(__v), 1)
 #define ATOMIC_INCREASE(__v, __n) (void) __sync_add_and_fetch(&(__v), (__n))
 #define ATOMIC_DECREASE(__v, __n) (void) __sync_sub_and_fetch(&(__v), (__n))
 #define ATOMIC_CAS(__v, __o, __n) __sync_bool_compare_and_swap(&(__v), (__o), (__n))
 #define ATOMIC_CAS_RETURN(__v, __o, __n) __sync_val_compare_and_swap(&(__v), (__o), (__n))
 
 #define ATOMIC_SET(__v, __n) {\
-    if (__builtin_expect(ATOMIC_READ(__v) != (__n), 1)) {\
-        int __b = 0;\
-        do {\
-            __b = ATOMIC_CAS(__v, ATOMIC_READ(__v), __n);\
-        } while (__builtin_expect(!__b, 0));\
-    }\
+    int __b = 0;\
+    do {\
+        __b = ATOMIC_CAS(__v, ATOMIC_READ(__v), __n);\
+    } while (__builtin_expect(!__b, 0));\
 }
 
 #define ATOMIC_SET_IF(__v, __c, __n, __t) {\
