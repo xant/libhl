6bd6ecbb7d7c4379e847b28f417f499001a9064a
Andrea Guzzo <xant@xant.net>
2014-06-12 13:19:44 +0200

    removed the cast to void in the macro definitions
    caller might still want to look at the return value

--- src/atomic_defs.h
+++ src/atomic_defs.h
@@ -2,10 +2,10 @@
 #define __ATOMIC_DEFS_H__
 
 #define ATOMIC_READ(__v) __sync_add_and_fetch(&(__v), 0)
-#define ATOMIC_INCREMENT(__v) (void) __sync_fetch_and_add(&(__v), 1)
-#define ATOMIC_DECREMENT(__v) (void) __sync_fetch_and_sub(&(__v), 1)
-#define ATOMIC_INCREASE(__v, __n) (void) __sync_add_and_fetch(&(__v), (__n))
-#define ATOMIC_DECREASE(__v, __n) (void) __sync_sub_and_fetch(&(__v), (__n))
+#define ATOMIC_INCREMENT(__v) __sync_fetch_and_add(&(__v), 1)
+#define ATOMIC_DECREMENT(__v) __sync_fetch_and_sub(&(__v), 1)
+#define ATOMIC_INCREASE(__v, __n) __sync_add_and_fetch(&(__v), (__n))
+#define ATOMIC_DECREASE(__v, __n) __sync_sub_and_fetch(&(__v), (__n))
 #define ATOMIC_CAS(__v, __o, __n) __sync_bool_compare_and_swap(&(__v), (__o), (__n))
 #define ATOMIC_CAS_RETURN(__v, __o, __n) __sync_val_compare_and_swap(&(__v), (__o), (__n))
 
