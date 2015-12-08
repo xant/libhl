61b949df28d3a6a4fff473a53e8524e7f30a87c7
Andrea Guzzo <xant@xant.net>
2015-12-07 16:49:36 -0800

    conform to last changes in the graph implementation

--- test/graph_test.c
+++ test/graph_test.c
@@ -34,8 +34,8 @@ int main(int argc, char **argv)
     else
         ut_failure("graph_node_connect() returned an error");
 
-    ut_testing("graph_node_connection_select with only 1 unconditional connection");
-    graph_node_t *test_node = graph_node_connection_select(node1);
+    ut_testing("graph_node_next with only 1 unconditional connection");
+    graph_node_t *test_node = graph_node_next(node1);
     ut_result(test_node == node2, "Can't get the unconditional connection");
 
     int select = 0;
@@ -48,12 +48,12 @@ int main(int argc, char **argv)
         ut_failure("graph_node_connect() returned an error");
 
     ut_testing("When the chooser returns false the new node is not selected");
-    test_node = graph_node_connection_select(node2);
+    test_node = graph_node_next(node2);
     ut_result(test_node == NULL, "The node was returned even if the chooser returned false");
 
     select = 1;
     ut_testing("When the chooser returns true the new node is not selected");
-    test_node = graph_node_connection_select(node2);
+    test_node = graph_node_next(node2);
     ut_result(test_node == node3, "The node has been selected when the chooser returned true");
 
     ut_testing("A node can be retrieved by label using graph_node_get()");
@@ -78,21 +78,21 @@ int main(int argc, char **argv)
         ut_failure("Returned nodes don't match");
 
     ut_testing("Connection selection");
-    test_node = graph_node_connection_select(node2);
+    test_node = graph_node_next(node2);
     select = 0;
     select2 = 1;
-    graph_node_t *test_node2 = graph_node_connection_select(node2);
+    graph_node_t *test_node2 = graph_node_next(node2);
     select2 = 0;
-    graph_node_t *test_node3 = graph_node_connection_select(node2);
+    graph_node_t *test_node3 = graph_node_next(node2);
 
     ut_result(test_node == node3 && test_node2 == node4 && test_node3 == node5, "Node selection doesn't behave as expected");
 
     ut_testing("Chooser weight is honored");
     select = 2;
     select2 = 1;
-    test_node = graph_node_connection_select(node2);
+    test_node = graph_node_next(node2);
     select2 = 3;
-    test_node2 = graph_node_connection_select(node2);
+    test_node2 = graph_node_next(node2);
     ut_result(test_node == node3 && test_node2 == node4, "Node selection doesn't honor weights returned by the chooser");
 
     graph_destroy(graph);
