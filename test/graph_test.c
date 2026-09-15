ae3e64484bbee53b2868b68cd7b92bf1bdbc18d2
Andrea Guzzo <xant@xant.net>
2026-09-15 15:09:48 +0200

    Add unit tests for graph node delete, disconnect and error reporting

--- test/graph_test.c
+++ test/graph_test.c
@@ -14,6 +14,15 @@ connection_chooser_test(graph_node_t *node, void *context)
     return 0;
 }
 
+static int free_value_calls = 0;
+
+static void
+test_free_value(void *value)
+{
+    free_value_calls++;
+    free(value);
+}
+
 int main(int argc, char **argv)
 {
     ut_init(basename(argv[0]));
@@ -21,9 +30,16 @@ int main(int argc, char **argv)
     graph_t *graph = graph_create("Test", NULL);
     ut_result(graph != NULL, "Can't create a new graph");
 
+    ut_testing("graph_errno() reports no error right after graph_create()");
+    ut_result(graph_errno(graph) == EGRAPHNOERR, "A freshly created graph should report EGRAPHNOERR");
+
     ut_testing("Adding a new node to the graph");
     graph_node_t *node1 = graph_node_add(graph, "start_node", "test", 5);
     ut_result(node1 != NULL && strcmp(graph_node_label_get(node1), "start_node") == 0, "Can't create a new node");
+
+    ut_testing("graph_node_add() doesn't crash and fails when label is NULL");
+    graph_node_t *bad_node = graph_node_add(graph, NULL, "x", 1);
+    ut_result(bad_node == NULL, "graph_node_add() should fail (not crash) when label is NULL");
 
     ut_testing("Adding a new node and connect (unconditionally) the first node to the new node");
     graph_node_t *node2 = graph_node_add(graph, "second_node", "test2", 6);
@@ -95,12 +111,82 @@ int main(int argc, char **argv)
     test_node2 = graph_node_next(node2);
     ut_result(test_node == node3 && test_node2 == node4, "Node selection doesn't honor weights returned by the chooser");
 
+    ut_testing("graph_node_disconnect() removes an existing connection");
+    rc = graph_node_disconnect(node2, node5);
+    ut_result(rc == 0, "graph_node_disconnect() failed to remove an existing connection");
+
+    ut_testing("A disconnected node is no longer among the returned connections");
+    graph_node_t *post_disconnect[3] = { NULL, NULL, NULL };
+    int post_disconnect_count = graph_node_connections_get(node2, post_disconnect, 3);
+    int found_node5 = 0;
+    int i;
+    for (i = 0; i < post_disconnect_count; i++) {
+        if (post_disconnect[i] == node5)
+            found_node5 = 1;
+    }
+    ut_result(post_disconnect_count == 2 && !found_node5, "node5 is still listed as a connection of node2 after being disconnected");
+
+    ut_testing("graph_node_disconnect() fails when the connection doesn't exist");
+    rc = graph_node_disconnect(node2, node5);
+    ut_result(rc == -1, "Disconnecting an already-disconnected pair should fail");
+
+    // restore the connection so the downstream delete test still sees node2 with 3 connections
+    graph_node_connect(node2, node5, NULL, NULL);
+
+    ut_testing("graph_node_connections_get() returns 0 for a node with no connections");
+    int no_connections = graph_node_connections_get(node3, NULL, 0);
+    ut_result(no_connections == 0, "A node with no outgoing connections should report 0 connections");
+
+    ut_testing("graph_node_add() replacing an existing label releases the old value via free_value_cb");
+    graph_t *vgraph = graph_create("ValueTest", test_free_value);
+    graph_node_add(vgraph, "dup", strdup("value1"), 7);
+    graph_node_t *dup_node = graph_node_add(vgraph, "dup", strdup("value2"), 7);
+    ut_result(dup_node != NULL && free_value_calls == 1, "Old value wasn't released when overwriting an existing node");
+
+    ut_testing("graph_node_delete() releases the node's value via free_value_cb");
+    graph_node_delete(vgraph, "dup", NULL, 0);
+    ut_result(free_value_calls == 2, "free_value_cb wasn't invoked when deleting a node");
+
+    ut_testing("graph_destroy() releases remaining values via free_value_cb");
+    graph_node_add(vgraph, "another", strdup("value3"), 7);
+    graph_destroy(vgraph);
+    ut_result(free_value_calls == 3, "free_value_cb wasn't invoked for a node still present at graph_destroy() time");
+
+    ut_testing("graph_node_delete() doesn't crash when graph is NULL");
+    int delete_ret = graph_node_delete(NULL, "start_node", NULL, 0);
+    ut_result(delete_ret == -1, "Delete should fail (not crash) when graph is NULL");
+
+    ut_testing("graph_node_delete() doesn't crash when label is NULL");
+    delete_ret = graph_node_delete(graph, NULL, NULL, 0);
+    ut_result(delete_ret == -1, "Delete should fail (not crash) when label is NULL");
+
+    ut_testing("Deleting a node that was never added fails");
+    delete_ret = graph_node_delete(graph, "no_such_node", NULL, 0);
+    ut_result(delete_ret == -1, "Delete should fail for a label never added to the graph");
+
+    ut_testing("graph_errno() reflects the error set by the failed delete");
+    ut_result(graph_errno(graph) == EGRAPHTABLEERR, "graph_errno() didn't report the expected error code");
+
+    ut_testing("Deleting a node returns its (possibly truncated) connections");
+    graph_node_t *deleted_connections[2] = { NULL, NULL };
+    delete_ret = graph_node_delete(graph, "second_node", deleted_connections, 2);
+    ut_result(delete_ret == 2 && deleted_connections[0] == node3 && deleted_connections[1] == node4,
+               "Delete didn't return the expected truncated list of connections");
+
+    ut_testing("A deleted node can no longer be retrieved from the graph");
+    ut_result(graph_node_get(graph, "second_node") == NULL, "The deleted node is still present in the graph");
+
+    ut_testing("Deleting an already-deleted node fails");
+    delete_ret = graph_node_delete(graph, "second_node", NULL, 0);
+    ut_result(delete_ret == -1, "Deleting the same node twice should fail the second time");
+
     ut_testing("Deleting an existing node from the graph");
-    int delete_ret;
     delete_ret = graph_node_delete(graph, "start_node", NULL, 100);
     ut_result(delete_ret == 0, "Delete failed");
 
+    ut_testing("graph_destroy() doesn't crash with dangling connections left by deleted nodes");
     graph_destroy(graph);
+    ut_success();
 
     ut_summary();
     exit(ut_failed);
